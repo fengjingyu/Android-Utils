9ded7bda8861af7cd88ba368df4b75876715c588
jingyu <fengjingyu@foxmail.com>
2017-02-24 01:08:27 +0800

    storager类补提交

--- utils/src/main/java/com/jingyu/utils/function/Storager.java
+++ utils/src/main/java/com/jingyu/utils/function/Storager.java
@@ -282,228 +282,228 @@ public class Storager {
             }
             return null;
         }
+    }
 
+    /**
+     * 外部存储--之公共目录,不会随app的删除而删除
+     * /storage/emulated/0/
+     * /storage/sdcard/
+     */
+    public static class ExternalPublic {
         /**
-         * 外部存储--之公共目录,不会随app的删除而删除
-         * /storage/emulated/0/
-         * /storage/sdcard/
+         * @return /storage/sdcard/
          */
-        public static class ExternalPublic {
-            /**
-             * @return /storage/sdcard/
-             */
-            public static File getDir() {
-                if (isSDcardExist()) {
-                    return Environment.getExternalStorageDirectory();
-                }
-                return null;
+        public static File getDir() {
+            if (isSDcardExist()) {
+                return Environment.getExternalStorageDirectory();
             }
-
-            /**
-             * @param dirName "aa" ,"aa/bb"
-             * @return /storage/sdcard/aa/bb
-             */
-            public static File getDir(String dirName) {
-                if (isSDcardExist()) {
-                    File dir = Environment.getExternalStoragePublicDirectory(dirName);
-                    // 不会自动创建的,得调用mkdirs()
-                    if (dir != null) {
-                        if (dir.exists() || dir.mkdirs()) {
-                            return dir;
-                        }
-                    }
-                }
-                return null;
-            }
+            return null;
         }
 
         /**
-         * 内部存储-会随app的删除而删除
+         * @param dirName "aa" ,"aa/bb"
+         * @return /storage/sdcard/aa/bb
          */
-        public static class Internal {
+        public static File getDir(String dirName) {
+            if (isSDcardExist()) {
+                File dir = Environment.getExternalStoragePublicDirectory(dirName);
+                // 不会自动创建的,得调用mkdirs()
+                if (dir != null) {
+                    if (dir.exists() || dir.mkdirs()) {
+                        return dir;
+                    }
+                }
+            }
+            return null;
+        }
+    }
 
-            /**
-             * @return fileName 为 null /"" /" ",返回null
-             * fileName不为空:
-             * dirName 为 null /"" /"  " 则在data/data/package/创建文件
-             * dirName 为"aa" 则在data/data/package/aa创建文件
-             * dirName 为"aa/bb" 则在data/data/package/aa/bb创建文件
-             */
-            public static File getFile(Context context, String dirName, String fileName) {
-                if (fileName == null && fileName.trim().length() == 0) {
-                    return null;
+    /**
+     * 内部存储-会随app的删除而删除
+     */
+    public static class Internal {
+
+        /**
+         * @return fileName 为 null /"" /" ",返回null
+         * fileName不为空:
+         * dirName 为 null /"" /"  " 则在data/data/package/创建文件
+         * dirName 为"aa" 则在data/data/package/aa创建文件
+         * dirName 为"aa/bb" 则在data/data/package/aa/bb创建文件
+         */
+        public static File getFile(Context context, String dirName, String fileName) {
+            if (fileName == null && fileName.trim().length() == 0) {
+                return null;
+            } else {
+                File dir = getDir(context, dirName);
+                if (dir != null) {
+                    return createFile(dir, fileName);
                 } else {
-                    File dir = getDir(context, dirName);
-                    if (dir != null) {
-                        return createFile(dir, fileName);
-                    } else {
-                        return null;
-                    }
+                    return null;
                 }
-            }
-
-            /**
-             * @return dirName=null / "" / "  ",返回/data/data/<package>/
-             * dirName="cc" 返回/data/data/<package>/cc
-             * dirName="aa/bb" 返回/data/data/<package>/aa/bb
-             */
-            public static File getDir(Context context, String dirName) {
-                if (dirName == null || dirName.trim().length() == 0) {
-                    return getPackageDir(context);
-                }
-                return createDir(getPackageDir(context).getAbsolutePath() + File.separator + dirName);
-            }
-
-            /**
-             * @param dirName 这个系统api只提供"aa" 或"bb"的目录 ,不可以是"aa/bb"带有分隔符的,否则非法参数异常;如果需要"aa/bb",可以获取路径后用普通方式创建多级目录
-             * @return dirName为"" 返回/data/data/<package>/app_ ,这个app_是系统创建目录的时候自带的
-             * dirName为null 返回/data/data/<package>/app_null,这个app_是系统创建目录的时候自带的
-             * dirName非空 返回/data/data/<package>/app_+dirName,这个app_是系统创建目录的时候自带的
-             */
-            public static File getAppDir(Context context, String dirName) {
-                return context.getDir(dirName, Context.MODE_PRIVATE);
-            }
-
-            /**
-             * @return /data/data/<package>/cache
-             */
-            public static File getCacheDir(Context context) {
-                return context.getCacheDir();
-            }
-
-            /**
-             * @return /data/data/<package>/files
-             */
-            public static File getFilesDir(Context context) {
-                return context.getFilesDir();
-            }
-
-            /**
-             * @return /data/data/<package>/
-             */
-            public static File getPackageDir(Context context) {
-                return context.getCacheDir().getParentFile();
-            }
-
-            /**
-             * 写文本(覆盖之前的文件)到内部存储,文件保存在/data/data/"PACKAGE_NAME"/files/fileName
-             *
-             * @param fileName 如 "android.txt" ,不可以是"aa/bb.txt",即不可以包含路径分隔符
-             */
-            public static void write2FilesDirPrivate(Context context, String fileName, String content) {
-                write2FilesDir(context, fileName, content, Context.MODE_PRIVATE);
-            }
-
-            /**
-             * 写文本(在之前文本后面接着写)到内部存储,文件保存在/data/data/"PACKAGE_NAME"/files/fileName
-             *
-             * @param fileName 如 "android.txt" ,不可以是"aa/bb.txt",即不可以包含路径分隔符
-             */
-            public static void write2FilesDirAppend(Context context, String fileName, String content) {
-                write2FilesDir(context, fileName, content, Context.MODE_APPEND);
-            }
-
-            private static void write2FilesDir(Context context, String fileName, String content, int mode) {
-                FileOutputStream fos = null;
-                BufferedWriter writer = null;
-                try {
-                    fos = context.openFileOutput(fileName, mode);
-                    writer = new BufferedWriter(new OutputStreamWriter(fos));
-                    writer.write(content);
-                    //fos.write(content.getBytes());
-                    //fos.flush();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (writer != null) {
-                            writer.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            /**
-             * 从内部存储读文件,文件在/data/data/"PACKAGE_NAME"/files/filename
-             *
-             * @param fileName 文件名 如 "android.txt" 不可以是"aa/bb.txt",系统只提供了"android.txt"方式的api
-             */
-            public static String readFromFilesDir(Context context, String fileName) {
-                FileInputStream in = null;
-                BufferedReader reader = null;
-                StringBuilder content = new StringBuilder();
-                try {
-                    in = context.openFileInput(fileName);
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    String line = "";
-                    while ((line = reader.readLine()) != null) {
-                        content.append(line);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (reader != null) {
-                            reader.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                return content.toString();
-            }
-
-            /**
-             * @return /system
-             */
-            public static File getSystemDir() {
-                return Environment.getRootDirectory();
-            }
-
-            /**
-             * @return /data
-             */
-            public static File getDataDir() {
-                return Environment.getDataDirectory();
-            }
-
-            /**
-             * @return /cache
-             */
-            public static File getCacheDir() {
-                return Environment.getDownloadCacheDirectory();
             }
         }
 
-        public static class IOStream {
+        /**
+         * @return dirName=null / "" / "  ",返回/data/data/<package>/
+         * dirName="cc" 返回/data/data/<package>/cc
+         * dirName="aa/bb" 返回/data/data/<package>/aa/bb
+         */
+        public static File getDir(Context context, String dirName) {
+            if (dirName == null || dirName.trim().length() == 0) {
+                return getPackageDir(context);
+            }
+            return createDir(getPackageDir(context).getAbsolutePath() + File.separator + dirName);
+        }
 
-            public static InputStream getInputStreamFromRaw(Context context, int rawId) {
+        /**
+         * @param dirName 这个系统api只提供"aa" 或"bb"的目录 ,不可以是"aa/bb"带有分隔符的,否则非法参数异常;如果需要"aa/bb",可以获取路径后用普通方式创建多级目录
+         * @return dirName为"" 返回/data/data/<package>/app_ ,这个app_是系统创建目录的时候自带的
+         * dirName为null 返回/data/data/<package>/app_null,这个app_是系统创建目录的时候自带的
+         * dirName非空 返回/data/data/<package>/app_+dirName,这个app_是系统创建目录的时候自带的
+         */
+        public static File getAppDir(Context context, String dirName) {
+            return context.getDir(dirName, Context.MODE_PRIVATE);
+        }
+
+        /**
+         * @return /data/data/<package>/cache
+         */
+        public static File getCacheDir(Context context) {
+            return context.getCacheDir();
+        }
+
+        /**
+         * @return /data/data/<package>/files
+         */
+        public static File getFilesDir(Context context) {
+            return context.getFilesDir();
+        }
+
+        /**
+         * @return /data/data/<package>/
+         */
+        public static File getPackageDir(Context context) {
+            return context.getCacheDir().getParentFile();
+        }
+
+        /**
+         * 写文本(覆盖之前的文件)到内部存储,文件保存在/data/data/"PACKAGE_NAME"/files/fileName
+         *
+         * @param fileName 如 "android.txt" ,不可以是"aa/bb.txt",即不可以包含路径分隔符
+         */
+        public static void write2FilesDirPrivate(Context context, String fileName, String content) {
+            write2FilesDir(context, fileName, content, Context.MODE_PRIVATE);
+        }
+
+        /**
+         * 写文本(在之前文本后面接着写)到内部存储,文件保存在/data/data/"PACKAGE_NAME"/files/fileName
+         *
+         * @param fileName 如 "android.txt" ,不可以是"aa/bb.txt",即不可以包含路径分隔符
+         */
+        public static void write2FilesDirAppend(Context context, String fileName, String content) {
+            write2FilesDir(context, fileName, content, Context.MODE_APPEND);
+        }
+
+        private static void write2FilesDir(Context context, String fileName, String content, int mode) {
+            FileOutputStream fos = null;
+            BufferedWriter writer = null;
+            try {
+                fos = context.openFileOutput(fileName, mode);
+                writer = new BufferedWriter(new OutputStreamWriter(fos));
+                writer.write(content);
+                //fos.write(content.getBytes());
+                //fos.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
                 try {
-                    return context.getResources().openRawResource(rawId);
-                } catch (Exception e) {
+                    if (writer != null) {
+                        writer.close();
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
-                    return null;
                 }
             }
+        }
 
-            public static InputStream getInputStreamFromAsserts(Context context, String fileName) {
+        /**
+         * 从内部存储读文件,文件在/data/data/"PACKAGE_NAME"/files/filename
+         *
+         * @param fileName 文件名 如 "android.txt" 不可以是"aa/bb.txt",系统只提供了"android.txt"方式的api
+         */
+        public static String readFromFilesDir(Context context, String fileName) {
+            FileInputStream in = null;
+            BufferedReader reader = null;
+            StringBuilder content = new StringBuilder();
+            try {
+                in = context.openFileInput(fileName);
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
                 try {
-                    return context.getAssets().open(fileName);
-                } catch (Exception e) {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
-                    return null;
                 }
             }
+            return content.toString();
+        }
 
-            public static InputStream getInputStreamFromUri(Context context, Uri uri) {
-                try {
-                    return context.getContentResolver().openInputStream(uri);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return null;
-                }
+        /**
+         * @return /system
+         */
+        public static File getSystemDir() {
+            return Environment.getRootDirectory();
+        }
+
+        /**
+         * @return /data
+         */
+        public static File getDataDir() {
+            return Environment.getDataDirectory();
+        }
+
+        /**
+         * @return /cache
+         */
+        public static File getCacheDir() {
+            return Environment.getDownloadCacheDirectory();
+        }
+    }
+
+    public static class IOStream {
+
+        public static InputStream getInputStreamFromRaw(Context context, int rawId) {
+            try {
+                return context.getResources().openRawResource(rawId);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public static InputStream getInputStreamFromAsserts(Context context, String fileName) {
+            try {
+                return context.getAssets().open(fileName);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public static InputStream getInputStreamFromUri(Context context, Uri uri) {
+            try {
+                return context.getContentResolver().openInputStream(uri);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
             }
         }
     }

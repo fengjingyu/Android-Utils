201a7042afa42a7e49fb9154754f570d2ab17e66
jingyu <fengjngyu@foxmail.com>
2017-07-27 11:54:35 +0800

    修改依赖

--- init/src/main/java/com/demo/init/WebViewActivity.java
+++ init/src/main/java/com/demo/init/WebViewActivity.java
@@ -16,8 +16,8 @@ public class WebViewActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_webview);
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
     }
 
     public void initWidgets() {
